466c6d4877bb57bf72eb5a8680c6f9d0c63ea096
Artem Zborovskyi <dhf532@its.york.ac.uk>
2025-09-29 12:16:09 +0100

    exercise 4

---
+++ ex4.py
@@ -0,0 +1,34 @@
+# Exercise 4:
+    # a. Write a script that takes a sentence from the user without any punctuation
+    # and prints the sentence without any white spaces. Note a white space is
+    # represented by ' ', and an empty string is represented by ''.
+    # >>> enter a sentence: this is a SHORT sentence
+    # thisisaSHORTsentence
+
+    # b. Same as above except that each word in the output should start with a
+    # upper case letter and all other letter should be lower case (also known as
+    # CamelCase).
+    # >>> enter a sentence: this is a SHORT sentence
+    # ThisIsAShortSentence
+
+    # c. Write a script that takes a sentence from a user written in CamelCase
+    # (without any blank spaces), creates the list of words from that sentence,
+    # and then prints that list.
+    # >>> enter a sentence in CamelCase: ThisIsAShortSentence
+    # ['This','Is','A','Short','Sentence']
+
+def has_punctuation(string):
+    for char in string:
+        if char.isalpha() == False and char != " ":
+            return False
+        
+    return True
+
+def remove_whitespaces():
+    string = input("enter a sentence: ")
+    if has_punctuation(string) == False:
+        return remove_whitespaces()
+
+    print(string.replace(" ", ""))
+
+remove_whitespaces()
